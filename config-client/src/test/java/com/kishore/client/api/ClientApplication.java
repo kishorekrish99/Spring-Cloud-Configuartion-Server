31b50b3e72a0c7250d75db7a836222d6d16cb9a8
uputoori kishore <48278073+kishorekrish99@users.noreply.github.com>
2020-07-01 01:07:40 +0530

    .

--- config-client/src/test/java/com/kishore/client/api/ClientApplication.java
+++ config-client/src/test/java/com/kishore/client/api/ClientApplication.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class InsuranceClientApplicationTests {
+public class ClientApplication {
 
 	@Test
 	public void contextLoads() {
